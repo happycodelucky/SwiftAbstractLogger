eda7f129dddc4fb2a100c0a62bca268a4eea2178
Paul Bates <paul.a.bates@gmail.com>
2017-02-14 00:24:31 -0800

    Initial Commit
    Initiall commit of the AbstractSwiftLogger package.
    
    * Added package sources
    * Updated README.md to include information on the package

---
+++ Sources/SwiftAbstractLogger.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAbstractLogger.h
+//  SwiftAbstractLogger
+//
+//  Created by Paul Bates on 2/13/17.
+//  Copyright © 2017 Paul Bates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftAbstractLogger.
+FOUNDATION_EXPORT double SwiftAbstractLoggerVersionNumber;
+
+//! Project version string for SwiftAbstractLogger.
+FOUNDATION_EXPORT const unsigned char SwiftAbstractLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAbstractLogger/PublicHeader.h>
+
+
